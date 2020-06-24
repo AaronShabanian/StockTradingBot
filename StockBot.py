7b9f26def32e0833eb7bf97306199b3407bf2092
AaronShabanian <shabanian@chapman.edu>
2020-06-24 13:23:03 -0700

    Adding when to sell

--- StockBot.py
+++ StockBot.py
@@ -3,6 +3,9 @@ import time
 import requests
 from yahoo_fin import stock_info as si
 positions=[0,0,0,0]
+buyPrice=[0,0,0,0]
+sellPrice=[0,0,0,0]
+shares=[0,0,0,0]
 #amzn=0 msft=1 goog=2 fb=3
 api = tradeapi.REST('PKQJOI25DAFM21LXXIYE', 'NL4hFAkbVPXmFLIqhzddXcZ1jWemq4zSBoOkQjWW', 'https://paper-api.alpaca.markets', api_version='v2')
 def trade():
@@ -41,11 +44,15 @@ def trade():
 
 
 def analyze(symbol, hist, index):
+    size=len(hist)
     account=api.get_account()
     balance=float(account.equity)
-    print("analyze")
     if positions[index]!=0:
-        sell(symbol, amount, index)
+        currPrice=si.get_live_price(symbol)
+        difference=currPrice-buyPrice[index];
+        percent= (difference/buyPrice[index])*100
+        if hist[size-1]<hist[size-2]:
+            sell(symbol, positions[index], index)
     elif positions[index]==0:
         print("Nothing owned")
 
@@ -59,7 +66,16 @@ def order(name, number, index):
         time_in_force='gtc'
     )
     positions[index]+=number;
-
+    #Checking the price you purchsed at
+    if index==0:
+        buyPrice[index]=si.get_live_price("amzn")
+    elif index==1:
+        buyPrice[index]=si.get_live_price("msft")
+    elif index==2:
+        buyPrice[index]=si.get_live_price("goog")
+    elif index==3:
+        buyPrice[index]=si.get_live_price("fb")
+    print("Purchased "+name+" Number of Shares: "+number+ " At $"+buyPrice[index]+ " Per Share")
 
 def sell(name, number, index):
     api.submit_order(
@@ -70,4 +86,19 @@ def sell(name, number, index):
         time_in_force='gtc'
     )
     positions[index]-=number;
+    if index==0:
+        sellPrice[index]=si.get_live_price("amzn")
+    elif index==1:
+        sellPrice[index]=si.get_live_price("msft")
+    elif index==2:
+        sellPrice[index]=si.get_live_price("goog")
+    elif index==3:
+        sellPrice[index]=si.get_live_price("fb")
+    print("Sold "+name+" Number of Shares: "+number+ " At $"+sellPrice[index]+ " Per Share")
+    gain=sellPrice[index]-buyPrice[index]
+    if gain>=0:
+        print("Sold at a gain of $"+gain+" Per Share")
+    else:
+        print("Sold at a loss of $"+gain+" Per Share")
+
 trade()
