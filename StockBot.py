dd40b68e65c2b2909f703c2adf73c1bcde8794a5
AaronShabanian <shabanian@chapman.edu>
2020-06-25 13:03:35 -0700

    Now a functional Stock Bot

--- StockBot.py
+++ StockBot.py
@@ -1,11 +1,16 @@
 import alpaca_trade_api as tradeapi
 import time
+import pytz
+import random
+import datetime
 import requests
 from yahoo_fin import stock_info as si
 positions=[0,0,0,0]
 buyPrice=[0,0,0,0]
 sellPrice=[0,0,0,0]
 shares=[0,0,0,0]
+print("Welcome to an Open Source Stock trading bot")
+print("You will be updated periodically on your account equity, purchases and buying power")
 #amzn=0 msft=1 goog=2 fb=3
 api = tradeapi.REST('PKQJOI25DAFM21LXXIYE', 'NL4hFAkbVPXmFLIqhzddXcZ1jWemq4zSBoOkQjWW', 'https://paper-api.alpaca.markets', api_version='v2')
 def trade():
@@ -19,26 +24,43 @@ def trade():
         googl=[]
         counter=0;
         while True:
-            counter+=1
-            #Getting live prices and storing them in an array to analyze
-            amzn=si.get_live_price("amzn")
-            msft=si.get_live_price("msft")
-            goog=si.get_live_price("goog")
-            fb=si.get_live_price("fb")
-            amznl.append(amzn)
-            msftl.append(msft)
-            googl.append(goog)
-            fbl.append(fb)
-            if counter>10:
-                amznl.pop(0)
-                msftl.pop(0)
-                googl.pop(0)
-                fbl.pop(0)
-            analyze(amzn,amznl,0)
-            analyze(msft, msftl, 1)
-            analyze(goog, googl, 2)
-            analyze(fb,fbl,3)
-            time.sleep(60)
+            if clock.is_open:
+                counter+=1
+                print(counter)
+                #Getting live prices and storing them in an array to analyze
+                amzn=si.get_live_price("amzn")
+                msft=si.get_live_price("msft")
+                goog=si.get_live_price("goog")
+                fb=si.get_live_price("fb")
+                amznl.append(amzn)
+                msftl.append(msft)
+                googl.append(goog)
+                fbl.append(fb)
+                if counter>10:
+                    amznl.pop(0)
+                    msftl.pop(0)
+                    googl.pop(0)
+                    fbl.pop(0)
+                if counter>10:
+                    analyze(amzn,amznl,0)
+                    analyze(msft, msftl, 1)
+                    analyze(goog, googl, 2)
+                    analyze(fb,fbl,3)
+                time.sleep(60)
+                if counter%10==0:
+                    account=api.get_account()
+                    balance=float(account.equity)
+                    power=account.buying_power
+                    print("The current time is: ")
+                    current_time = datetime.datetime.now(pytz.timezone('US/Eastern'))
+                    print (current_time)
+                    print("Current Stock Buying Power is: $")
+                    print(power)
+                    print("Current Total Equity is: $")
+                    print(balance)
+            else:
+                print("Market has Closed")
+                break;
     else:
         print("Market is Currently Closed")
 
@@ -49,12 +71,19 @@ def analyze(symbol, hist, index):
     balance=float(account.equity)
     if positions[index]!=0:
         currPrice=si.get_live_price(symbol)
-        difference=currPrice-buyPrice[index];
+        difference=currPrice-buyPrice[index]
         percent= (difference/buyPrice[index])*100
+        current_time = datetime.datetime.now(pytz.timezone('US/Eastern'))
         if hist[size-1]<hist[size-2]:
             sell(symbol, positions[index], index)
+        elif current_time.hour==3 and current_time.minute==58:
+            sell(symbol, positions[index], index)
     elif positions[index]==0:
-        print("Nothing owned")
+        power=account.buying_power
+        if hist[size-1]<hist[size-9] and hist[size-1]>hist[size-2] and power>currPrice:
+            maxShares=int(power/currPrice)
+            purchaseNum=random.randrange(1,maxShares)
+            order(sybol,purchaseNum,index)
 
 
 def order(name, number, index):
